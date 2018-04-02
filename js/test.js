b89e10493de2a600aa0faa74ffb51e0e912d9b1e
Derek Blue <derekmblue4011@gmail.com>
2018-04-02 15:23:00 -0300

    Working on banner/header javascript

--- js/test.js
+++ js/test.js
@@ -1,17 +1,63 @@
+// Document Elements
+var header;
+var banner;
+
+// Dimension Variables
 var vh = $(window).height();
 var vw = $(window).width();
+var headerWidth;
+var headerHeight;
+var bannerWidth;
+var bannerHeight;
 
-var debug = $("#debugLog");
+// Debug log zones
+var debugHeaderWidth = $("#headerWidth");
+var debugHeaderHeight = $("#headerHeight");
+var debugBannerWidth = $("#bannerWidth");
+var debugBannerHeight = $("#bannerHeight");
+var debugCurrentState = $("#bannerState");
+var debugScrollPosition = $("#scrollPosition");
 
+// Scroll wrapper
 var wrap = $("#mainWrapper");
 var mainBanner = $("#mainBanner");
 
+$('document').ready(function(){
+	header = document.getElementById('mainHeader');
+	banner = document.getElementById('mainBanner');
+	headerWidth = header.clientWidth;
+	headerHeight = header.clientHeight;
+	bannerWidth = banner.clientWidth;
+	bannerHeight = banner.clientHeight;
+	debugHeaderWidth.text(headerWidth);
+	debugHeaderHeight.text(headerHeight);
+	debugBannerWidth.text(bannerWidth);
+	debugBannerHeight.text(bannerHeight);
+	debugCurrentState.text("Banner displayed");
+	debugScrollPosition.text("0");
+});
+
+$(window).bind("resize", function() {
+	vh = $(window).height();
+	vw = $(window).width();
+	headerWidth = header.clientWidth;
+	headerHeight = header.clientHeight;
+	bannerWidth = banner.clientWidth;
+	bannerHeight = banner.clientHeight;
+	debugHeaderWidth.text(headerWidth);
+	debugHeaderHeight.text(headerHeight);
+	debugBannerWidth.text(bannerWidth);
+	debugBannerHeight.text(bannerHeight);
+});
+
 wrap.on("scroll", function(e) {
-    debug.text(this.scrollTop)
-	if (this.scrollTop > 700 - (0.15*vh)) {
-		mainBanner.addClass("scrollHiddenBanner");
-		debug.text("Threshold passed:");
+	debugScrollPosition.text(this.scrollTop);
+	if (this.scrollTop > (bannerHeight - headerHeight - 4)) {
+		debugCurrentState.text("Banner displayed");
+		mainBanner.addClass("headerBanner");
+		mainBanner.style.transform("translate(0, ${headerHeight - bannerHeight + 4})");
 	} else {
-		mainBanner.removeClass("scrollHiddenBanner");
+		debugCurrentState.text("Banner in header");
+		mainBanner.removeClass("headerBanner");
 	}
 });
