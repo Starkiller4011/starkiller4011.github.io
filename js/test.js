73a3961cd4b4953987db4374c29127acd9db93ac
Derek Blue <derekmblue4011@gmail.com>
2018-04-06 10:39:48 -0300

    started work on bio

--- js/test.js
+++ js/test.js
@@ -1,5 +1,4 @@
 $("#mainWrapper").on("scroll", function(e) {
-	$("#topLink").css("opacity", this.scrollTop / (document.getElementById('mainBanner').clientHeight - document.getElementById('mainHeader').clientHeight - 4));
 	$("#mainHeaderBG").css("opacity", this.scrollTop / (document.getElementById('mainBanner').clientHeight - document.getElementById('mainHeader').clientHeight - 4));
 	if (this.scrollTop > (document.getElementById('mainBanner').clientHeight - document.getElementById('mainHeader').clientHeight - 4)) {
 		$("#mainHeaderBanner").addClass("headerBannerStateTwo");
